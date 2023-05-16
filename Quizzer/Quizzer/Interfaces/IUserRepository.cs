a80621759637d0ff7afe900dc70ee6763e6f2bfb
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-05-16 19:27:43 +0100

    added user repository

---
+++ Quizzer/Quizzer/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+using Quizzer.Models.Entities;
+
+namespace Quizzer.Interfaces;
+
+public interface IUserRepository
+{
+    public User? GetByUsername(string? username);
+}
---
+++ Quizzer/Quizzer/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using Quizzer.Data;
+using Quizzer.Interfaces;
+using Quizzer.Models.Entities;
+
+namespace Quizzer.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly QuizzerContext _db;
+
+    public UserRepository(QuizzerContext db)
+    {
+        _db = db;
+    }
+    
+    public User? GetByUsername(string? username)
+    {
+        return _db.User.FirstOrDefault(u =>
+            username != null && u.Username != null && u.Username.ToLower() == username.ToLower()
+        );
+    }
+}
