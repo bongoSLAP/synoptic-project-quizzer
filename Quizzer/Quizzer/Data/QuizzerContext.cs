f87219df328f2034d97dc154f4ece109c70ff035
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-05-16 12:42:32 +0100

    setup mysql host and entityframework

---
+++ Quizzer/Quizzer/Data/QuizzerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quizzer.Data;
+
+public class QuizzerContext : DbContext
+{
+    public QuizzerContext (DbContextOptions<QuizzerContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+    }
+}
