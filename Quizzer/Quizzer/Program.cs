d70936f14fbe48e27a0ef36f06f2af08b6a757e6
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-05-16 19:26:54 +0100

    setup jwt auth

--- Quizzer/Quizzer/Program.cs
+++ Quizzer/Quizzer/Program.cs
@@ -1,7 +1,24 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Quizzer.Data;
+using Quizzer.Handlers;
+using Quizzer.Interfaces;
+using Quizzer.Repositories;
+using Quizzer.Wrappers;
+using Scrypt;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ILoginHandler, LoginHandler>();
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+builder.Services.AddSingleton<ScryptEncoder, ScryptEncoder>();
+builder.Services.AddSingleton<IScryptEncoder, ScryptEncoderWrapper>();
+
+builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString(name: "QuizzerContext");
 
@@ -9,6 +26,22 @@ builder.Services.AddDbContext<QuizzerContext>(options =>
 {
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        }
+    );
 
 var app = builder.Build();
 
@@ -21,12 +54,16 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
-app.MapRazorPages();
+app.UseAuthentication();
 
 app.Run();
