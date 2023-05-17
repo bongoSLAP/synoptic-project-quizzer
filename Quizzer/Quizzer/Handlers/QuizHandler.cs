aa7e192524ecc18f5c11feef40af6dd819c44bdf
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-05-17 15:02:15 +0100

    added quiz handler and endpoint

---
+++ Quizzer/Quizzer/Handlers/QuizHandler.cs
@@ -0,0 +1,25 @@
+﻿using Quizzer.Interfaces;
+using Quizzer.Models.Entities;
+using Quizzer.Models.Enums;
+
+namespace Quizzer.Handlers;
+
+public class QuizHandler : IQuizHandler
+{
+    private readonly IQuizRepository _quizRepository;
+    
+    public QuizHandler(IQuizRepository quizRepository)
+    {
+        _quizRepository = quizRepository;
+    }
+    
+    public IEnumerable<Quiz> List(Role role)
+    {
+        var quizzes = role == Role.Student ? _quizRepository.List() : _quizRepository.ListIncludeAnswers();
+        
+        if (!quizzes.Any())
+            throw new InvalidOperationException("No quizzes found");
+
+        return quizzes;
+    }
+}
